e47757b8197c6b2f7744f70bc7032f9900ec993c
Artem Kopytko <artem.kop19@gmail.com>
2023-07-26 14:50:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _112._Path_Sum
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null) return false;
+            if (root.left == null && root.right == null && targetSum == root.val) return true;
+            return HasPathSum(root.left,targetSum - root.val) || HasPathSum(root.right,targetSum - root.val);
+    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
